03fc45139cde500bd1aeed5cce239ef543145f01
Richard Cardoso <richard.166@hotmail.com>
2023-10-21 03:56:04 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  cloud {
+      organization = projeto-fiap-64"
+      workspaces {
+        name = "projeto-fiap-iac-infra"
+      }
+    }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
