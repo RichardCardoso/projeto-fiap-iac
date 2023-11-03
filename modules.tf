579c71ea171611a306700ad07ca3719ebe959c12
Richard Cardoso <richard.166@hotmail.com>
2023-11-03 16:26:05 -0300

    terraform vars 4

--- modules.tf
+++ modules.tf
@@ -14,10 +14,10 @@ module "rds" {
   eks_subnet_public_1a = module.network.eks_subnet_public_1a
   vpc_id = module.network.vpc_id
 
-  db_cluster_name = var.db_cluster_name;
-  db_name = var.db_name;
-  db_username = var.db_username;
-  db_password = var.db_password;
+  db_cluster_name = var.db_cluster_name
+  db_name = var.db_name
+  db_username = var.db_username
+  db_password = var.db_password
 }
 
 # module "master" {
