29d18490863e29231566751cd957a46a342c810c
Richard Cardoso <richard.166@hotmail.com>
2023-11-05 22:42:33 -0300

    lambda test 29

--- modules/api-gateway/api-gateway.tf
+++ modules/api-gateway/api-gateway.tf
@@ -15,7 +15,9 @@ resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
-  authorization = "NONE"
+
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.projeto_fiap_authorizer.id
 
   request_parameters = {
     "method.request.path.proxy" = true
