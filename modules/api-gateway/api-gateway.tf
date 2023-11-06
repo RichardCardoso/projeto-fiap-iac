4da1b0227019b23d017aca76d0c39958a1aa87fb
Richard Cardoso <richard.166@hotmail.com>
2023-11-05 21:55:11 -0300

    lambda test 24

--- modules/api-gateway/api-gateway.tf
+++ modules/api-gateway/api-gateway.tf
@@ -30,7 +30,7 @@ resource "aws_api_gateway_resource" "proxy" {
 
 resource "aws_api_gateway_method" "auth" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.proxy.id
+  resource_id   = aws_api_gateway_resource.auth.id
   http_method   = "POST"
   authorization = "NONE"
 }
