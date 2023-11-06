e16540b2e3d66def92764e1fc0c103e05c6bce89
Richard Cardoso <richard.166@hotmail.com>
2023-11-05 23:19:30 -0300

    lambda test 32

--- modules/api-gateway/api-gateway.tf
+++ modules/api-gateway/api-gateway.tf
@@ -66,7 +66,7 @@ resource "aws_lambda_permission" "api_gateway_auth" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_auth_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*/${aws_api_gateway_method.proxy.http_method}${aws_api_gateway_resource.proxy.path}"
+  source_arn    = "${aws_api_gateway_rest_api.this.execution_arn}/*"
 }
 
 resource "aws_api_gateway_integration" "this" {
