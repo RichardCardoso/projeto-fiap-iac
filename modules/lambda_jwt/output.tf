28ec8a65dc3603f959c0f03662d48945582a150c
Richard Cardoso <richard.166@hotmail.com>
2023-11-05 22:04:08 -0300

    lambda test 26

--- modules/lambda_jwt/output.tf
+++ modules/lambda_jwt/output.tf
@@ -1,4 +1,4 @@
 output "lambda_invoke_arn" {
-  value = aws_lambda_function.terraform_lambda_auth.invoke_arn
+  value = aws_lambda_function.terraform_lambda_jwt.invoke_arn
   description = "O ARN usado para invocar a função Lambda"
 }
